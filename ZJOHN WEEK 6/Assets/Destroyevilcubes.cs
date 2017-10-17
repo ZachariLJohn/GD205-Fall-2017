90b43538177ef3dfe0448cfa3144c28c9b8533ed
Zachari L. John <riamlawiet@gmail.com>
2017-10-17 00:11:16 -0400

    week6ce3

---
+++ ZJOHN WEEK 6/Assets/Destroyevilcubes.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyevilcubes : MonoBehaviour {
+    public GameObject destroyMe1;
+    public GameObject destroyMe2;
+    public GameObject destroyMe3;
+    public GameObject destroyMe4;
+    public GameObject destroyMe5;
+    public GameObject destroyMe6;
+    public GameObject destroyMe7;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+           
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bye1"))
+        {
+            Destroy(destroyMe1);
+        }
+
+        if (other.gameObject.CompareTag("Bye2"))
+        {
+            Destroy(destroyMe2);
+        }
+
+        if (other.gameObject.CompareTag("Bye3"))
+        {
+            Destroy(destroyMe3);
+        }
+
+        if (other.gameObject.CompareTag("Bye4"))
+        {
+            Destroy(destroyMe4);
+        }
+
+        if (other.gameObject.CompareTag("Bye5"))
+        {
+            Destroy(destroyMe5);
+        }
+
+        if (other.gameObject.CompareTag("Bye6"))
+        {
+            Destroy(destroyMe6);
+        }
+
+        if (other.gameObject.CompareTag("Bye7"))
+        {
+            Destroy(destroyMe7);
+        }
+
+
+    }
+}
