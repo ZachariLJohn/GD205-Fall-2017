1080e71beaafc7efafc962eec47cb5b537a35427
Zachari L. John <riamlawiet@gmail.com>
2017-10-30 03:49:05 -0400

    test

---
+++ Week 7 Hw/Assets/Loadscene2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Loadscene2 : MonoBehaviour {
+
+    public void LoadByIndex(int sceneIndex)
+
+    {
+
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+}
---
+++ Week 7 Hw/Assets/TImer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TImer : MonoBehaviour {
+    public int timeLeft = 500;
+    public Text countdownText;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("LoseTime");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        countdownText.text = ("Time Left = " + timeLeft);
+        if (timeLeft <= 0)
+        {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times Up!";
+            Application.LoadLevel("Day cycle");
+        }
+    }
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
